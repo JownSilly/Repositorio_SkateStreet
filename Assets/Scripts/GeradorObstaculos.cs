8002aba58820048657bfc630733fecc7e99024b5
JownSilly <joaovitordealmeidaojeda2@hotmail.com>
2022-06-20 18:29:58 -0300

    Gerador Obstaculos funcional

--- Assets/Scripts/GeradorObstaculos.cs
+++ Assets/Scripts/GeradorObstaculos.cs
@@ -1,49 +1,79 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class GeradorObstaculos : MonoBehaviour
 {
-    public GameObject [] cloneObs;
+    public GameObject[] cloneObs;
 
     public GameObject coin;
     public GameObject boost;
     public GameObject personagem;
     private int i;
+
     public float delayInicial, delayObj, delayObj2;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
-        InvokeRepeating("GerarClone", delayInicial, delayObj);
-        InvokeRepeating("GerarCloneCoin", delayInicial, delayObj2);
-        InvokeRepeating("GerarCloneBoost", 10, 20f);
+        InvokeRepeating(nameof(GerarClone), delayInicial, delayObj);
+        InvokeRepeating(nameof(GerarCloneCoin), delayInicial, delayObj2);
+        InvokeRepeating(nameof(GerarCloneBoost), 10, 20f);
     }
-    // Update is called once per frame
-    void GerarClone()
+
+    private void GerarClone()
     {
         i = Random.Range(0, cloneObs.Length);
-        float x = personagem.transform.position.x+10f;
-        float y = cloneObs[i].transform.position.y;
+        var x = personagem.transform.position.x + 10f;
+        var y = cloneObs[i].transform.position.y;
         var spawnObs = new Vector2(x, y);
-        Instantiate(cloneObs[i], spawnObs,Quaternion.identity);
+        Instantiate(cloneObs[i], spawnObs, Quaternion.identity);
     }
-    
-    void GerarCloneCoin()
+
+    private static bool IsPositionAwayFromObstacles(Vector2 spawnCol)
     {
-        float x = personagem.transform.position.x + 12f;
-        float y = -3.2f;
-        var spawnCol = new Vector2(x, y);       
-        Instantiate(coin, spawnCol, Quaternion.identity);
+        var obstaculos = GameObject.FindGameObjectsWithTag("Obstacle");
+
+        foreach (var obstaculo in obstaculos)
+        {
+            var distancia = Vector2.Distance(obstaculo.transform.position, spawnCol);
+
+            if (distancia < 2)
+            {
+                return true;
+            }
+        }
+
+        return false;
     }
-    void GerarCloneBoost()
+
+    private void GerarCloneCoin()
     {
-        float x = personagem.transform.position.x + 13f;
-        float y = -3.3f;
-        var spawnCol = new Vector2(x, y);
-        //posicaoAleatoria = CheckDistanciaObstaculos();
-        Instantiate(boost, spawnCol, Quaternion.identity);
+        var x = personagem.transform.position.x + 12f;
+        var y = -3.2f;
+
+        var spawnPosition = new Vector2(x, y);
+
+        // Pegar todos os objetos que são inimigos (com a tag Obstacle)
+        // Se a distância entre todos os objetos e o spawnCol for menor que 2, não spawna o objeto
+        if (IsPositionAwayFromObstacles(spawnPosition))
+        {
+            return;
+        }
+
+        Instantiate(coin, spawnPosition, Quaternion.identity);
     }
-   
-    
+
+    private void GerarCloneBoost()
+    {
+        var x = personagem.transform.position.x + 13f;
+        var y = -3.3f;
+
+        var spawnPosition = new Vector2(x, y);
+
+        if (IsPositionAwayFromObstacles(spawnPosition))
+        {
+            return;
+        }
+
+        Instantiate(boost, spawnPosition, Quaternion.identity);
+    }
 }
